b4547564b556148ba29eefe9751794ea82304c2e
Sandra Noelia Rodriguez Escobar <sandranoeliarodriguezescobar@gmail.com>
2022-11-10 17:52:11 -0300

    Antes de la tarea 4

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors= require('cors');
+
+const servidor= express();
+
+servidor.use(morgan('dev'));
+servidor.use(express.json());
+servidor.use(cors());
+
+servidor.get("/", (peti, resp)=>{
+    resp.send("Hola desde servidor REST");
+});
+
+servidor.use ("/libro", require('./rutas/ruta-libro'));
+servidor.use ("/autor", require('./rutas/ruta-autor'));
+
+
+servidor.listen(3000, ()=>{
+    console.log("Servidor escuchando en el puerto 3000");
+});
---
+++ rutas/libro.js
@@ -0,0 +1,12 @@
+const LIBRO = [
+    {id: 10, titulo: "Cien años de soledad", autor:"Gabriel García Márquez.", paginas:"250"},
+    {id: 11, titulo: "El señor de los anillos (Trilogía)", autor:"J. R. R. Tolkien", paginas:"1000"},
+    {id: 12, titulo: "Orgullo y prejuicio", autor:"Jane Austen", paginas:"200"},
+    {id: 13, titulo: "Lolita", autor:"Vladimir Nabokov", paginas:"300"},
+    {id: 14, titulo: "Ulises", autor:"James Joyce", paginas:"300"},
+    {id: 15, titulo: "El retrato de Dorian Gray", autor:"Oscar Wilde", paginas:"250"},
+    {id: 16, titulo: "El Principito", autor:"Antoine de Saint-Exupéry", paginas:"100"},
+    {id: 17, titulo: "La Odisea", autor:"Homero", paginas:"300"},
+];
+
+module.exports = LIBRO;
